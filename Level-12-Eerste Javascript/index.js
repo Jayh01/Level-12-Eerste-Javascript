204241c98fd176f224a7f050fa65948925426fd2
Jayh01 <84378798+Jayh01@users.noreply.github.com>
2021-07-31 23:05:14 +0200

    Add files via upload

---
+++ Level-12-Eerste Javascript/index.js
@@ -0,0 +1,32 @@
+console.log('Hello Winc Academy');
+let naam = 'Joybert';
+console.log(naam);
+
+let = ' (if) gaat niet want naat is al gereserveerd';
+console.log()
+
+let optelsom = 4 + 4;
+console.log(optelsom);
+
+let optelsom2 = '4' + '4';
+console.log(optelsom2);
+
+let test1 = 200 + 365;
+console.log(test1);
+
+let test2 = 648 - 249;
+console.log(test2);
+
+let test3 = 16 * 357;
+console.log(test3)
+
+let test4 = 5712 / 16;
+console.log(test4)
+
+let test5 = 4 % 500;
+console.log(test5)
+
+let mijnLeefTijd = '48';
+console.log(mijnLeefTijd)
+
+// uitkomst van '48'= Nummer
